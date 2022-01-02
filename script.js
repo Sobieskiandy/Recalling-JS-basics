21495e115741317aa80f30f8559ce701a3e46155
Piotr Lada <sobieskiandy@gmail.com>
2022-01-02 21:09:47 +0100

    All files for now, maybe later I'll give sth more, or change code, dunno

---
+++ script.js
@@ -0,0 +1,10 @@
+function a(){
+    document.getElementById("here").src="pic_bulbon.gif"
+    console.log(5+5)
+}
+function b(){
+    document.getElementById("here").src="pic_bulboff.gif"
+}
+function multiply(a,b){
+    document.getElementById("GARY").innerHTML=a*b;
+}
